9b35a2ed73a55b9af8360069e047e59f6b4e0b3b
nikita <zypab2006@gmail.com>
2024-10-21 19:39:47 +0300

    STATIC_ROOT

--- smotrelka/smotrelka/settings.py
+++ smotrelka/smotrelka/settings.py
@@ -116,8 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static_root')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static', 'static_files'),)
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
