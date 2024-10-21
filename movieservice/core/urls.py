36daf46216cee2ffc550aa27fdd17ee0d42e037a
Arsenii Karpov <abs2016123@gmail.com>
2024-10-21 21:03:29 +0300

    Change names

--- movieservice/core/urls.py
+++ movieservice/core/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for smotrelka project.
+URL configuration for movieservice project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -23,4 +23,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("homepage.urls")),
+    path("url/", include("url_handler.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
