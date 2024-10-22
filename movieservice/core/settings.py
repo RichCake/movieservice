65db4f6fb2057546560c727f93ac92ccaf9a4f24
Arsenii Karpov <abs2016123@gmail.com>
2024-10-22 14:39:17 +0300

    settings hotfix

--- movieservice/core/settings.py
+++ movieservice/core/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 ##
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = "/static/"
 
 # Default primary key field type
