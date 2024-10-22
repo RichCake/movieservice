66d0f080503be15459e9a316e9f821ecc0afb600
tapo44ek <136156340+tapo44ek@users.noreply.github.com>
2024-10-22 09:05:22 +0300

    Update settings.py - STATICFILE_DIRS added

--- movieservice/core/settings.py
+++ movieservice/core/settings.py
@@ -116,6 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+## Добавил строчку для пути на статику
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+##
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = "/static/"
 
