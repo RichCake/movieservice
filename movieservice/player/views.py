69c507f9d46e42c80b6996f6ff06369a61730514
Arsenii Karpov <abs2016123@gmail.com>
2024-10-28 16:30:11 +0300

    add features that send and accept movie data

--- movieservice/player/views.py
+++ movieservice/player/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+import json
+
 
 def player_view(request):
-    return render(request, "player/index.html", {"data": '"{\"kinopoisk\":\"4626783\",\"title\":\"«Пчеловод» (The Beekeeper, 2024)\"}"'})
+    movie_data = json.dumps({"kinopoisk": "4626783", "title": "«Пчеловод» (The Beekeeper, 2024)"})
+
+    return render(request, "player/index.html", {"data": movie_data})
