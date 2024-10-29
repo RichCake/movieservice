39889e34ad930c66fb200b6c284fa0238c6751d5
Aleksandr <Aleksandr260206@yandex.ru>
2024-10-29 15:19:33 +0300

    add icon for browsers

--- movieservice/homepage/tests.py
+++ movieservice/homepage/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
 
-# Create your tests here.
+
+class HomepageTests(SimpleTestCase):
+    # Проверка на работу страницы
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    # Тест на применение верных шаблонов
+    def test_template_name_correct(self):
+        response = self.client.get('')
+        self.assertTemplateUsed(response, "homepage/main.html")
