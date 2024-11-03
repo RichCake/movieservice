20a5995f998853f2b14c40a9e6b0ba029514cfb5
Arsenii Karpov <abs2016123@gmail.com>
2024-11-03 18:30:18 +0300

    improve user exp

--- movieservice/player/views.py
+++ movieservice/player/views.py
@@ -1,11 +1,5 @@
-import json
-
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
-from movie_history.models import History
 
 
-@login_required
 def player_view(request):
     return render(request, "player/index.html", {"data": None})
