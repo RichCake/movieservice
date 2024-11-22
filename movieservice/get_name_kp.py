190b789909430868b20ecdb84b2d13345c54fa93
tapo44ek <136156340+tapo44ek@users.noreply.github.com>
2024-11-22 13:57:11 +0300

    Update get_name_kp.py (chrome_options)

--- movieservice/get_name_kp.py
+++ movieservice/get_name_kp.py
@@ -10,6 +10,10 @@ from webdriver_manager.chrome import ChromeDriverManager
 def get_kp_name_by_link(link):
     chrome_options = Options()
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")  # Уменьшение использования памяти
+    chrome_options.add_argument("--no-sandbox")  # Запуск без песочницы
+    chrome_options.add_argument("--disable-gpu")  # Отключение GPU (не нужно в headless)
+    chrome_options.add_argument("--remote-debugging-port=9222")  # Устранение ошибки DevToolsActivePort
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     try:
         driver.get(link)
